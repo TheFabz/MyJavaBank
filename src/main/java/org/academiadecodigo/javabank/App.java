87c5eb99113d198a28f34a80399a86b4d14c5833
UbuntuFabz <ffrr.rodrigues@gmail.com>
2021-03-23 10:20:24 +0000

    updated with spring

--- src/main/java/org/academiadecodigo/javabank/App.java
+++ src/main/java/org/academiadecodigo/javabank/App.java
@@ -20,44 +20,13 @@ public class App {
 
     public static void main(String[] args) {
 
-        /*JpaBootstrap jpa = new JpaBootstrap();
-        EntityManagerFactory emf = jpa.start();
-
-        JpaSessionManager sm = new JpaSessionManager(emf);
-        TransactionManager tx = new JpaTransactionManager(sm);
-
-        App app = new App();
-        app.bootStrap(tx, sm);*/
-
         ApplicationContext ac = new ClassPathXmlApplicationContext("/META-INF/spring-config.xml");
-
+        
         Controller controller = ac.getBean("loginController", LoginController.class);
 
         controller.init();
 
-       // jpa.stop();
 
     }
 
-    private void bootStrap(TransactionManager tx, JpaSessionManager sm) {
-
-       /* AccountServiceImpl accountService = new AccountServiceImpl();
-        accountService.setAccountDao(new JpaAccountDao(sm));
-        accountService.setTransactionManager(tx);
-
-        CustomerServiceImpl customerService = new CustomerServiceImpl();
-        customerService.setCustomerDao(new JpaCustomerDao(sm));
-        customerService.setTransactionManager(tx);
-
-        Bootstrap bootstrap = new Bootstrap();
-
-        bootstrap.setAuthService(new AuthServiceImpl());
-        bootstrap.setAccountService(accountService);
-        bootstrap.setCustomerService(customerService);
-
-        Controller controller = bootstrap.wireObjects();*/
-
-        // start application
-        //controller.init();
-    }
 }
